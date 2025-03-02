9e022f2de7bb7235ebcd34238b0e22551b64375b
Grace <yxc081224@gmail.com>
2025-03-02 12:02:19 +0800

    新增skill功能的POST請求

--- week4/server.js
+++ week4/server.js
@@ -169,7 +169,7 @@ const requestListener = async (req, res) => {
   } else if (req.url === "/api/coaches/skill" && req.method === "GET") {
     try {
       const skills = await AppDataSource.getRepository("Skill").find({
-        select: ["id", "name", "createdAt"],
+        select: ["id", "name"],
       });
       res.writeHead(200, headers);
       res.write(
@@ -190,6 +190,68 @@ const requestListener = async (req, res) => {
       res.end();
     }
   } else if (req.url === "/api/coaches/skill" && req.method === "POST") {
+    req.on("end", async () => {
+      try {
+        const data = JSON.parse(body);
+        if (
+          isUndefined(data.name) ||
+          isNotValidSting(data.name) ||
+          isUndefined(data.credit_amount) ||
+          isNotValidInteger(data.credit_amount) ||
+          isUndefined(data.price) ||
+          isNotValidInteger(data.price)
+        ) {
+          res.writeHead(400, headers);
+          res.write(
+            JSON.stringify({
+              status: "failed",
+              message: "欄位未填寫正確",
+            })
+          );
+          res.end();
+          return;
+        }
+        const skillRepo = await AppDataSource.getRepository("SKILL");
+        const existSkill = await skillRepo.find({
+          where: {
+            name: data.name,
+          },
+        });
+        if (existSkill.length > 0) {
+          res.writeHead(409, headers);
+          res.write(
+            JSON.stringify({
+              status: "failed",
+              message: "資料重複",
+            })
+          );
+          res.end();
+          return;
+        }
+        const newSkill = await skillRepo.create({
+          name: data.name,
+        });
+        const result = await skillRepo.save(newSkill);
+        res.writeHead(200, headers);
+        res.write(
+          JSON.stringify({
+            status: "success",
+            data: result,
+          })
+        );
+        res.end();
+      } catch (error) {
+        console.error(error);
+        res.writeHead(500, headers);
+        res.write(
+          JSON.stringify({
+            status: "error",
+            message: "伺服器錯誤",
+          })
+        );
+        res.end();
+      }
+    });
   } else if (
     req.url.startsWith("/api/credit-package/:creditPackageId/") &&
     req.method === "DELETE"
